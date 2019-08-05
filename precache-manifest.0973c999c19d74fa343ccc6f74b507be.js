671b9a5205032cc365010584fcfe8d6a2d2b09c4
jchen <jchen@cheweilai.cn>
2019-08-05 14:11:26 +0800

    Updates

---
+++ precache-manifest.0973c999c19d74fa343ccc6f74b507be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9d95912fd75af7e99363b4e3ea36da",
+    "url": "/easy_write/index.html"
+  },
+  {
+    "revision": "45cae439fd61dca9c055",
+    "url": "/easy_write/static/css/2.83f3aabf.chunk.css"
+  },
+  {
+    "revision": "46a37c9315237e39683e",
+    "url": "/easy_write/static/css/main.4d9103c7.chunk.css"
+  },
+  {
+    "revision": "45cae439fd61dca9c055",
+    "url": "/easy_write/static/js/2.c8bc336f.chunk.js"
+  },
+  {
+    "revision": "46a37c9315237e39683e",
+    "url": "/easy_write/static/js/main.8d8a6384.chunk.js"
+  },
+  {
+    "revision": "c0e9914fae6aae28452b",
+    "url": "/easy_write/static/js/runtime~main.b46ac17f.js"
+  }
+]);
